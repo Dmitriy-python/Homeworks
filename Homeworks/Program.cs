3fb7930ef0943e6afabf9e7719028378e5f1df57
Дмитрий <d03608665@gmail.com>
2025-04-25 00:49:32 +0300

    Initial commit

---
+++ Homeworks/Program.cs
@@ -0,0 +1,50 @@
+﻿using HtmlAgilityPack;
+
+
+// Задача с парсером валют и энумератором
+Main(CurrencyType.USD);
+void Main(CurrencyType type)
+{
+        // Загрузка HTML-документа
+        var url = "https://www.rbc.ru/quote/ticker/72413"; // Укажите URL
+        HtmlWeb web = new HtmlWeb();
+        var doc = web.Load(url);
+
+        // Пример: получение всех заголовков h1
+        if (doc.DocumentNode != null)
+        {
+            switch (type)
+            {
+                case CurrencyType.USD:
+                {
+                    HtmlNode? currencyusdNode =
+                        doc.DocumentNode.SelectSingleNode(
+                            "//*[@id=\"maincontent\"]/div/div[2]/div[1]/div[1]/div[1]/div/div[2]/span[1]/text()[2]");
+                    Console.WriteLine($"\t{HtmlEntity.DeEntitize(currencyusdNode.InnerText)}");
+                    break;
+                }
+                case CurrencyType.EUR:
+                {
+                    HtmlNode? currencyeurNode =
+                        doc.DocumentNode.SelectSingleNode(
+                            "//*[@id=\"maincontent\"]/div/div[2]/div[1]/div[1]/div[1]/div/div[2]/span[1]");
+                    Console.WriteLine($"\t{HtmlEntity.DeEntitize(currencyeurNode.InnerText)}");
+                    break;
+                }
+
+            }
+        }
+}
+enum CurrencyType {
+        USD,
+        EUR,
+        GBP,
+        JPY
+}
+
+
+
+
+
+
+
