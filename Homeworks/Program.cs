b5562608418a2859a6c1b3a9552dc11a2897568a
Vladislav <gogoha.hug.8172@gmail.com>
2025-04-25 17:35:42 +0300

    Final version corrected

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -3,13 +3,16 @@
 
 // Задача с парсером валют и энумератором
 
-// Main(CurrencyType.USD);
+// Main(CurrencyType.EUR);
 // void Main(CurrencyType type)
 // {
 //         
-//         var url = "https://www.rbc.ru/quote/ticker/72413"; 
+//         var url = "https://www.rbc.ru/quote/ticker/72413";
+//         
+//         var url2 = "https://www.rbc.ru/quote/ticker/72383";
 //         HtmlWeb web = new HtmlWeb();
 //         var doc = web.Load(url);
+//         var doc2 = web.Load(url2);
 //         
 //            
 //         if (doc.DocumentNode != null)
@@ -20,14 +23,14 @@
 //                 {
 //                     HtmlNode? currencyusdNode =
 //                         doc.DocumentNode.SelectSingleNode(
-//                             "//*[@id=\"maincontent\"]/div/div[2]/div[1]/div[1]/div[1]/div/div[2]/span[1]/text()[2]");
+//                             "//*[@id=\"maincontent\"]/div/div[2]/div[1]/div[1]/div[1]/div/div[2]/span[1]");
 //                     Console.WriteLine($"\t{HtmlEntity.DeEntitize(currencyusdNode.InnerText)}");
 //                     break;
 //                 }
 //                 case CurrencyType.EUR:
 //                 {
 //                     HtmlNode? currencyeurNode =
-//                         doc.DocumentNode.SelectSingleNode(
+//                         doc2.DocumentNode.SelectSingleNode(
 //                             "//*[@id=\"maincontent\"]/div/div[2]/div[1]/div[1]/div[1]/div/div[2]/span[1]");
 //                     Console.WriteLine($"\t{HtmlEntity.DeEntitize(currencyeurNode.InnerText)}");
 //                     break;
@@ -39,14 +42,14 @@
 
 
 //  рекурсия фибоначи
-int Fibonacci(int x)
-{
-   int result = 0;
-   if (x==0 || x==1) return x;
-   return result + Fibonacci(x - 1) + Fibonacci(x - 2);
-}
-int a = Fibonacci(6);
-Console.WriteLine(a);
+// int Fibonacci(int x)
+// {
+//    int result = 0;
+//    if (x==0 || x==1) return x;
+//    return result + Fibonacci(x - 1) + Fibonacci(x - 2);
+// }
+// int a = Fibonacci(6);
+// Console.WriteLine(a);
 
 
 // enum CurrencyType {
