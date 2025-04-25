55e978cac11ce9fe4421d54253a8e237aba6c2e0
Дмитрий <d03608665@gmail.com>
2025-04-25 13:03:24 +0300

    ss

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -2,49 +2,56 @@
 
 
 // Задача с парсером валют и энумератором
-Main(CurrencyType.USD);
-void Main(CurrencyType type)
+// Main(CurrencyType.USD);
+// void Main(CurrencyType type)
+// {
+//         // Загрузка HTML-документа
+//         var url = "https://www.rbc.ru/quote/ticker/72413"; // Укажите URL
+//         HtmlWeb web = new HtmlWeb();
+//         var doc = web.Load(url);
+//
+//         // Пример: получение всех заголовков h1
+//         if (doc.DocumentNode != null)
+//         {
+//             switch (type)
+//             {
+//                 case CurrencyType.USD:
+//                 {
+//                     HtmlNode? currencyusdNode =
+//                         doc.DocumentNode.SelectSingleNode(
+//                             "//*[@id=\"maincontent\"]/div/div[2]/div[1]/div[1]/div[1]/div/div[2]/span[1]/text()[2]");
+//                     Console.WriteLine($"\t{HtmlEntity.DeEntitize(currencyusdNode.InnerText)}");
+//                     break;
+//                 }
+//                 case CurrencyType.EUR:
+//                 {
+//                     HtmlNode? currencyeurNode =
+//                         doc.DocumentNode.SelectSingleNode(
+//                             "//*[@id=\"maincontent\"]/div/div[2]/div[1]/div[1]/div[1]/div/div[2]/span[1]");
+//                     Console.WriteLine($"\t{HtmlEntity.DeEntitize(currencyeurNode.InnerText)}");
+//                     break;
+//                 }
+//
+//             }
+//         }
+// }
+
+//  рекурсия фибоначи
+int Fibonacci(int x)
 {
-        // Загрузка HTML-документа
-        var url = "https://www.rbc.ru/quote/ticker/72413"; // Укажите URL
-        HtmlWeb web = new HtmlWeb();
-        var doc = web.Load(url);
-
-        // Пример: получение всех заголовков h1
-        if (doc.DocumentNode != null)
-        {
-            switch (type)
-            {
-                case CurrencyType.USD:
-                {
-                    HtmlNode? currencyusdNode =
-                        doc.DocumentNode.SelectSingleNode(
-                            "//*[@id=\"maincontent\"]/div/div[2]/div[1]/div[1]/div[1]/div/div[2]/span[1]/text()[2]");
-                    Console.WriteLine($"\t{HtmlEntity.DeEntitize(currencyusdNode.InnerText)}");
-                    break;
-                }
-                case CurrencyType.EUR:
-                {
-                    HtmlNode? currencyeurNode =
-                        doc.DocumentNode.SelectSingleNode(
-                            "//*[@id=\"maincontent\"]/div/div[2]/div[1]/div[1]/div[1]/div/div[2]/span[1]");
-                    Console.WriteLine($"\t{HtmlEntity.DeEntitize(currencyeurNode.InnerText)}");
-                    break;
-                }
-
-            }
-        }
+   int result = 0;
+   if (x==0 || x==1) return x;
+   return result + Fibonacci(x - 1) + Fibonacci(x - 2);
 }
-enum CurrencyType {
-        USD,
-        EUR,
-        GBP,
-        JPY
-}
+int a = Fibonacci(6);
+Console.WriteLine(a);
 
 
-
-
-
+// enum CurrencyType {
+//         USD,
+//         EUR,
+//         GBP,
+//         JPY
+// }
 
 
